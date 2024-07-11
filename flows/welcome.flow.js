50edbed2e50871cac5d91a9681612d8fea35b140
Jetsa05 <164280725+Jetsa05@users.noreply.github.com>
2024-07-11 17:01:57 -0500

    Delete flows directory

--- flows/welcome.flow.js
+++
@@ -1,8 +0,0 @@
-const { addKeyword, EVENTS } = require('@bot-whatsapp/bot');
-
-const welcomeFlow = addKeyword(EVENTS.ACTION)
-    .addAction(async (ctx, ctxFn) => {
-        await ctxFn.endFlow("Bienvenido a este chatbot! \nPodes escribir 'Agendar cita' para reservar una cita")
-    })
-
-module.exports = { welcomeFlow };
